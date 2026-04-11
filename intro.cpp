fe418478272335a65ca49f2fa23e6dd31fe09fa5
sitabja biswas <sitabjabiswas633@gmail.com>
2026-04-11 21:44:41 +0530

    new files

---
+++ intro.cpp
@@ -0,0 +1,29 @@
+//  intro to  dsa 
+
+
+
+// time complexity how the execution time of an algorithm changes as the size of the input changes 
+
+
+//  space complexity how the memory usage of an algorithm changes as the size of the input changes 
+
+// asymptotic notation =>  the  mathematial notation used to describe the behaviour of ann algorithm as the input size grows 
+
+
+
+//  there are 3 parts => average case=> the expected time taken by an algorithm for a random input of size n
+//  best case => the minimum time taken by an algorithm for any input of size n
+// worst case => the maximum time taken by an algorithm for any input of size n
+
+//  big O notation =>  used to describe the upper bound of an algorithm's time complexity
+//  big omega notation => used to describe the lower bound of an algorithm's time complexity
+//  big theta notation => used to describe the tight bound of an algorithm's time complexity
+
+
+// 0(1)=> constant time => the execution time of an algorithm does not depend on the size of the input
+// 0(log n) => logarithmic time => the execution time of an algorithm increases logarithmically
+// 0(n) => linear time => the execution time of an algorithm increases linearly with the size of the input
+// 0(n log n) => linearithmic time => the execution time of an algorithm increases as n log n
+// 0(n^2) => quadratic time => the execution time of an algorithm increases quadratically with the size of the input
+// 0(2^n) => exponential time => the execution time of an algorithm increases exponentially with the size of the input
+// 0(n!) => factorial time => the execution time of an algorithm increases factorially with the size
