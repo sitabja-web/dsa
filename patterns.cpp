d15ab3d747cafaa51c1c4b55ed0f088f342fb766
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-10 12:53:58 +0530

    nww files

--- patterns.cpp
+++ patterns.cpp
@@ -4,6 +4,19 @@
 using namespace std;
 
 
+
+// for(rows) {
+
+//     for(columns/spaces) {
+
+//     }
+
+//     cout << endl;
+// }
+
+//  this is the blue print 
+
+
 // * * * *
 // * * * *
 // * * * *
@@ -18,11 +31,15 @@ using namespace std;
 // outer loop = rows
 // inner loop = fixed stars
 
-for(int i=1;i<=4;i++) {
+for  (int i=1;i<=4;i++) {
+
+       //  outer loops for the rows
 
     for(int j=1;j<=4;j++) {
+       //   inner loops for the stars 
         cout<<"* ";
     }
 
     cout<<endl;
 }
+
