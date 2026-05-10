15fd910af7749f3dadbb54ad6027011020859e94
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-10 12:44:36 +0530

    new files

---
+++ patterns.cpp
@@ -0,0 +1,26 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// * * * *
+// * * * *
+// * * * *
+// * * * *
+
+
+// 4 rows
+// Every row has 4 stars
+
+// So:
+
+// outer loop = rows
+// inner loop = fixed stars
+
+int main ()  {
+       
+}
+
+//   patterns 
+
