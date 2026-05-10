154286633cf62a948bc842a24daad95c46fe5ff9
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-10 12:46:31 +0530

    new files

--- patterns.cpp
+++ patterns.cpp
@@ -18,9 +18,11 @@ using namespace std;
 // outer loop = rows
 // inner loop = fixed stars
 
-int main ()  {
-       
+for(int i=1;i<=4;i++) {
+
+    for(int j=1;j<=4;j++) {
+        cout<<"* ";
+    }
+
+    cout<<endl;
 }
-
-//   patterns 
-
