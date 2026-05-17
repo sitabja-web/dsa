479fa96a071d34ee8026aa17ba0584fa6d09f36d
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-17 21:56:02 +0530

    new files

--- patterns.cpp
+++ patterns.cpp
@@ -44,3 +44,24 @@ using namespace std;
 // }
 
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+*
+* *
+* * *
+* * * *
+* * * * *
