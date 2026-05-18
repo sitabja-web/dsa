89a0fd12d2f224a9bd997d0d568aceb913825edc
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-19 00:45:24 +0530

    new files

---
+++ hashing.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    //precompute:
+    int hash[13] = {0};
+    for (int i = 0; i < n; i++) {
+        hash[arr[i]] += 1;
+    }
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int number;
+        cin >> number;
+        // fetching:
+        cout << hash[number] << endl;
+    }
+    return 0;
+}
+
+
+
+// using hashmap 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    //precompute:
+    map<int, int> mp;
+    for (int i = 0; i < n; i++) {
+        mp[arr[i]]++;
+    }
+
+    // iterate over the map:
+    /* for(auto it : mp){
+            cout << it.first << "->" << it.second << endl;
+        }
+    */
+
+    int q;
+    cin >> q;
+    while (q--) {
+        int number;
+        cin >> number;
+        // fetch:
+        cout << mp[number] << endl;
+    }
+    return 0;
+}
