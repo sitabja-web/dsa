f4d0bd597fcaec1bbd3a8340c241d00cf83a8f00
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-19 01:09:21 +0530

    new files

--- hashing.cpp
+++ hashing.cpp
@@ -1,67 +1,126 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-int main() {
+// int main() {
 
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
 
-    //precompute:
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]] += 1;
-    }
+//     //precompute:
+//     int hash[13] = {0};
+//     for (int i = 0; i < n; i++) {
+//         hash[arr[i]] += 1;
+//     }
 
-    int q;
-    cin >> q;
-    while (q--) {
-        int number;
-        cin >> number;
-        // fetching:
-        cout << hash[number] << endl;
-    }
-    return 0;
-}
+//     int q;
+//     cin >> q;
+//     while (q--) {
+//         int number;
+//         cin >> number;
+//         // fetching:
+//         cout << hash[number] << endl;
+//     }
+//     return 0;
+// }
 
 
+Definition of Hashing:
+In order to optimize this approach, we need to use hashing. If we say the definition of hashing in a naive way, it is nothing but the combination of the steps, pre-storing, and fetching.
+
+Now, let’s understand how to solve the given problem using the two steps:
+Assumption: We are assuming that the maximum element in the given array can be 12.
+
+Step 1 - Pre-storing: In this step, we will create an array(named hash array) of size 13(so that we can get the index 12) initialized with 0. In this hash array, we are going to store the frequency of each element(i.e. The number of times each element appears in the array) of the given array. To do so, we will iterate over the given array, and for each element arr[i], we will do hash[arr[i]] += 1. After completing this process the hash array will look like the following:
+
+
+This step is named pre-storing as we are pre-calculating the information about the element of the array before answering the queries.
+
+Step 2 - Fetching: In this step, we will select each query i.e. the number and for the query, we will just fetch the value of hash[number] and return it instead of running a ‘for loop’ every time. 
+
+The implementation is given below:
+
+Time Complexity analysis of the function:
+We have learned how to compute the time complexity of any code. The above function contains a for loop that runs for N times. So, the time complexity of the function will be O(N) ignoring the other constant operations. 
+
+Now, for each query, we are calling this function. So, if the query contains 5 numbers and we call the function 5 times, the total time complexity will be O(5*N). If the number of queries is Q, the time complexity will be O(Q*N). 
+
+Now, if the length of the query becomes large like 105 and the array size also becomes large like 105, the time complexity will be O(1010).
+
+We know from our previous knowledge that 108 operations take 1 second to get executed. So, 1010 operations will take around 100 seconds(1010/108). We cannot say a code is good if it takes 100 seconds to get executed.
+
+Until now, we have learned the method of number hashing but using this method we cannot hash large numbers like 109 or higher. We can solve this problem using the STL map and unordered_map in C++ or the HashMap in Java collection. Now, we are going to discuss these concepts in detail and most of the concepts are the same in map/unordered_map(in C++) and HashMap(in Java).
+
+
+now the time complecity of the unordered map is 0(logn)  becaues under the hood it is implented usng a balanced binary tree but the time complexity of the unordered map is O(1) because under the hood it is implemented using a hash table. So, if we use an unordered map instead of a map, the time complexity of fetching will be O(1) and the overall time complexity will be O(N + Q) which is much better than O(Q*N).
 
 // using hashmap 
 
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// int main() {
+
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
+
+//     //precompute:
+//     map<int, int> mp;
+//     for (int i = 0; i < n; i++) {
+//         mp[arr[i]]++;
+//     }
+
+//     // iterate over the map:
+//     /* for(auto it : mp){
+//             cout << it.first << "->" << it.second << endl;
+//         }
+//     */
+
+//     int q;
+//     cin >> q;
+//     while (q--) {
+//         int number;
+//         cin >> number;
+//         // fetch:
+//         cout << mp[number] << endl;
+//     }
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-
     int n;
     cin >> n;
+
     int arr[n];
-    for (int i = 0; i < n; i++) {
+
+    // Input array
+    for(int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    //precompute:
-    map<int, int> mp;
-    for (int i = 0; i < n; i++) {
-        mp[arr[i]]++;
+    // Map for frequency counting
+    map<int, int> freq;
+
+    // Count frequency
+    for(int i = 0; i < n; i++) {
+        freq[arr[i]]++;
     }
 
-    // iterate over the map:
-    /* for(auto it : mp){
-            cout << it.first << "->" << it.second << endl;
-        }
-    */
-
-    int q;
-    cin >> q;
-    while (q--) {
-        int number;
-        cin >> number;
-        // fetch:
-        cout << mp[number] << endl;
+    // Print frequencies
+    for(auto it : freq) {
+        cout << it.first << " -> " << it.second << endl;
     }
+
     return 0;
 }
+
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,28 @@
 #include <bits/stdc++.h>
-using namespace std;
+// using namespace std;
 
-int main() {
+// int main() {
 
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
-    }
+//     int n;
+//     cin >> n;
+//     int arr[n];
+//     for (int i = 0; i < n; i++) {
+//         cin >> arr[i];
+//     }
 
-    //precompute:
-    int hash[13] = {0};
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]] += 1;
-    }
+//     //precompute:
+//     int hash[13] = {0};
+//     for (int i = 0; i < n; i++) {
+//         hash[arr[i]] += 1;
+//     }
 
-    int q;
-    cin >> q;
-    while (q--) {
-        int number;
-        cin >> number;
-        // fetching:
-        cout << hash[number] << endl;
-    }
-    return 0;
-}
+//     int q;
+//     cin >> q;
+//     while (q--) {
+//         int number;
+//         cin >> number;
+//         // fetching:
+//         cout << hash[number] << endl;
+//     }
+//     return 0;
+// }
