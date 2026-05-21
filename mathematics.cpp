4f091668c584065da47ed4ba2085a7d05155f306
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-22 00:13:03 +0530

    new files

--- mathematics.cpp
+++ mathematics.cpp
@@ -20,3 +20,30 @@ int main() {
 
     return 0;
 }
+
+
+// reverse a number 
+
+#include <iostream>
+using namespace std;
+
+int reverseNumber(int n) {
+    int rev = 0;
+
+    while (n > 0) {
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        n = n / 10;
+    }
+
+    return rev;
+}
+
+int main() {
+    int num;
+    cin >> num;
+
+    cout << reverseNumber(num);
+
+    return 0;
+}
