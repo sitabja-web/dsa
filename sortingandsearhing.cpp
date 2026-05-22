55a3d6300493dde16f7bdd9732ce24411a8e5240
sitabja biswas <sitabjabiswas633@gmail.com>
2026-05-22 16:58:48 +0530

    new files

--- sortingandsearhing.cpp
+++ sortingandsearhing.cpp
@@ -18,6 +18,25 @@ int main() {
 }
 
 //  vector way
-vector<int> v = {4, 1, 7, 2};
+// vector<int> v = {4, 1, 7, 2};
 
-sort(v.begin(), v.end());
+// sort(v.begin(), v.end());
+
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+
+    int arr[] = {9, 5, 2, 8, 1, 7};
+
+    // Sort from index 1 to index 4
+    sort(arr + 1, arr + 5);
+
+    for (int i = 0; i < 6; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
